c4ea71c94de436c9bfe22a46957d0cd90fe2898a
souravsgithub <souravsemail2001@gmail.com>
2024-06-09 00:21:23 +0530

    finally debugging is working in a java file

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,13 +1,13 @@
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = '/path/to/workspace-root' .. project_name
+local workspace_dir = '/home/sourav/nvme/java-projects' .. project_name
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
     -- 💀
-    '/usr/bin/java', -- or '/path/to/java17_or_newer/bin/java'
+    '/home/sourav/.sdkman/candidates/java/22.0.1-oracle/bin/java', -- or '/path/to/java17_or_newer/bin/java'
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -36,7 +36,7 @@ local config = {
 
     -- 💀
     -- See `data directory configuration` section in the README
-    '-data', workspace_dir
+    '-data', workspace_dir,
   },
 
   -- 💀
@@ -64,7 +64,7 @@ local config = {
   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
   init_options = {
     bundles = {
-      "/home/sourav/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.50.0.jar"
+      "/home/sourav/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.52.0.jar"
     }
   },
 }
