73096a933be1df5ba444a8a4f331a030f21c0571
sourav <souravkumarw845@gmail.com>
2025-12-29 15:49:39 +0530

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,15 @@
 -- Uncomment this if you want rose-pine / don't forget to set the alpha to 0.9 in st terminal
--- local M = {
---     "rose-pine/neovim",
---     name = "rose-pine"
--- }
--- M.config = function()
--- 	require("rose-pine").setup({
---         styles = {
---             transparency = true,
---         }
--- 	})
--- 	vim.cmd.colorscheme "rose-pine"
--- end
--- return M
-
-
--- local M = {
---     "rebelot/kanagawa.nvim"
--- }
---
--- M.config = function()
---     require("kanagawa").setup({})
---     vim.cmd.colorscheme("kanagawa")
--- end
-
-local M = { "ellisonleao/gruvbox.nvim",
-    priority = 1000 ,
-    config = true,
-    opts = ...
+local M = {
+    "rose-pine/neovim",
+    name = "rose-pine"
 }
-
 M.config = function()
-    require("gruvbox").setup({
-        vim.cmd.colorscheme("gruvbox")
-    })
+	require("rose-pine").setup({
+        styles = {
+            transparency = true,
+        }
+	})
+	vim.cmd.colorscheme "rose-pine"
 end
 return M
+
