fd3e5881238c79aec2d9b23533e3781414f8b0f9
sourav <souravkumarw845@gmail.com>
2026-01-27 16:47:43 +0530

    some more changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,5 +12,10 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+-- for deleting permanently
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- other keymaps
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
